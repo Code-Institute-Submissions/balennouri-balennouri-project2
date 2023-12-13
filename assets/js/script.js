3a7cf79165f07090930e1c5c6a16f1a34394088b
balennouri <balennouri@gmail.com>
2023-12-13 19:04:24 +0100

    change some class names and did some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 // Get elements by id
 
+const startBtnQuiz = document.getElementById('start-quiz-btn')
+const welcomeRules = document.getElementById('welcome-rules')
+const quizArea = document.getElementById('quiz-area')
+
 // Variables that change during the quiz
 
 
 // Quiz structure
 
+startBtnQuiz.addEventListener('click', startGame)
+
 function startGame() {
+console.log('started')
 
 }
 
