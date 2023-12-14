a055e39be6cdd7b97c801e0fe64d3e41a62f2f8c
balennouri <balennouri@gmail.com>
2023-12-14 13:07:53 +0100

    Add defer to questions.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,12 @@
 const welcomeRules = document.getElementById('welcome-rules')
 const startQuizBtn = document.getElementById('start-quiz-btn')
 const quizArea = document.getElementById('quiz-area')
+const answerButtonElement = document.getElementById('answer-button')
 
 
 startQuizBtn.addEventListener('click', startGame)
 
-// Quiz structure
+// Quiz game structure
 
 function startGame() {
     welcomeRules.classList.add('hide')
@@ -18,8 +19,6 @@ function setNextQuestion() {
 
 function showQuestion() {
 }
-
-
 
 function selectAnswer() {
 
